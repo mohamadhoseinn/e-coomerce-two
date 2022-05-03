40c40f7beefb93dcf1cf7f6bf78f7b0ffa145832
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-05-03 19:03:17 +0430

    create Button-component and use in  signup-page

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -5,6 +5,9 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/Firebase";
 import FormInput from "../form-input/FormInput";
+import Button from "../button/Button";
+
+import "./SignUpForm.scss";
 
 const defaultFormFields = {
   dispalyName: "",
@@ -52,8 +55,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign Up with your email and password</h1>
+    <div className="sign-up-continer">
+      <h2>Don't have an account?</h2>
+      <span>Sign Up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -88,7 +92,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
