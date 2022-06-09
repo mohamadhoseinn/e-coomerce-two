9ffd8945d83dbc13c632d71de6dd45bdfb84c9f5
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-06-09 18:11:57 +0430

    create function addCartItem use cartItems instead Of array into Cart Dropdown

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/CartContext";
 import Button from "../button/Button";
 import CartItem from "../cart-item/CartItem";
 
 import "./CartDropdown.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {[].map((item) => (
-          <CartItem cartItem={item} />
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
       <Button>GO TO CHECKOUT</Button>
