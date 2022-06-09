4737b6f4fcf40ff490e0410e626af91932eb00ce
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-06-09 16:49:07 +0430

    create cartItem and import in cartDropDown

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,11 +1,16 @@
 import Button from "../button/Button";
+import CartItem from "../cart-item/CartItem";
 
 import "./CartDropdown.scss";
 
 const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {[].map((item) => (
+          <CartItem cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
