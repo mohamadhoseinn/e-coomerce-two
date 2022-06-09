9ce4ec78c950a7d2fd137fda9e3b86f132c9f33f
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-06-09 18:06:00 +0430

    To make function addCartItem in file CartContext

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -2,8 +2,20 @@ import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems containers productToAdd
+  const exsistingCartItem = cartItems.find(
+    (cartItem) => cartItem?.id === productToAdd?.id
+  );
   // if found, increment quantity
+  if (exsistingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem?.id === productToAdd?.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
   // return new array with modified cartItems/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
 export const CartContext = createContext({
@@ -22,7 +34,7 @@ const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
