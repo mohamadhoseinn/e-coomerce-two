dd657a7f4c9ff36da14611ca905f4b7765a2e754
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-06-09 18:15:00 +0430

    use addItemToCart for click on Add-To-Card To be added to CartDropddown

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/CartContext";
 import Button from "../button/Button";
 
 import "./ProductCard.scss";
 
 const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +17,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add To Card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add To Card
+      </Button>
     </div>
   );
 };
