35f0c58d00d43861be2877b50be379952a5ff0d4
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-06-24 00:48:48 +0430

    add addCartItem with arrow-right and removeCartItem with arrow-left in checkout

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -12,13 +12,24 @@ const CheckoutItem = ({ cartItem }) => {
 
   const clearCartItemHandler = () => clearItemFromCart(cartItem);
 
+  const removeCartHandler = () => removeItemToCart(cartItem);
+  const addCartHandler = () => addItemToCart(cartItem);
+
   return (
     <div className="checkout-item-container">
       <div className="image-container">
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeCartHandler}>
+          &#10094;
+        </div>
+        <span>{quantity}</span>
+        <div className="arrow" onClick={addCartHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={clearCartItemHandler}>
         &#10005;
