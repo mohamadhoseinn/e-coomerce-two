ac22b803327fc64095db061a25229c92aae43b38
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-07-01 22:37:35 +0430

    use of styledComponent in CartDropDown

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -5,7 +5,11 @@ import { CartContext } from "../../contexts/CartContext";
 import Button from "../button/Button";
 import CartItem from "../cart-item/CartItem";
 
-import "./CartDropdown.scss";
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./CartDropdownStyle";
 
 const CartDropdown = () => {
   const { cartItems, setIsCartOpen } = useContext(CartContext);
@@ -17,14 +21,16 @@ const CartDropdown = () => {
     setIsCartOpen(false);
   };
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty </EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
