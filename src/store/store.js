369772b89afa5ed62469a30c34de8e147f10a8ad
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-07-15 01:22:25 +0430

    create setting in redux and use applyMiddleware and  compose for making Multiple middleware Like logger

--- src/store/store.js
+++ src/store/store.js
@@ -5,3 +5,11 @@ import {
 } from "redux";
 
 import { logger } from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+const middlewares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
