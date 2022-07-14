c5223831353e9a394c238a70f245cd73a3be719f
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-07-15 01:02:37 +0430

    install redux and create file store and root-reducer and user-reducer

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import {
+  compose,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from "redux";
+
+import { logger } from "redux-logger";
---
+++ src/store/user/user-reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  currentUser: null,
+};
+
+export const USER_ACTION_TYPE = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+export const userReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPE?.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      return state;
+  }
+};
